1d2f79b902f2dbf702253cddac73a1e636916960
theandygross <the.andrew.gross@gmail.com>
2013-10-23 15:27:10 -0700

    clean up notebooks

--- HNSCC_Manuscript/src/Processing/Imports.py
+++ HNSCC_Manuscript/src/Processing/Imports.py
@@ -7,19 +7,25 @@ import os as os
 import pickle as pickle
 import pandas as pd
 
-from Reports.NotebookTools import *
+#from Reports.NotebookTools import *
 from Stats.Scipy import *
 from Stats.Survival import *
 #from Reports.Figures import *
 from Processing.Helpers import *
+from Figures.Helpers import *
 from Figures.Pandas import *
 from Figures.Boxplots import *
-from Figures.R_Wrappers import *
-from Figures.Survival import draw_survival_curve
+#from Figures.R_Wrappers import *
+from Figures.Survival import draw_survival_curve, survival_and_stats
 
 pd.set_option('precision',3)
 pd.set_option('display.line_width', 100)
 pd.set_option('display.width', 300)
+plt.rcParams['font.size'] = 12
+
+'''Color schemes for paper taken from http://colorbrewer2.org/'''
+colors_st = ['#CA0020','#F4A582','#92C5DE', '#0571B0']
+colors_th = ['#E66101','#FDB863','#B2ABD2', '#5E3C99']
 
 def get_run(firehose_dir, version='Latest'):
     '''
