8d4746153d0f8caaf911ebac8899c62c69f4d5ed
theandygross <the.andrew.gross@gmail.com>
2014-02-13 14:40:54 -0800

    Fixed mut_filter to filter feature vectors with nearly all ones.

--- src/Processing/Screen.py
+++ src/Processing/Screen.py
@@ -7,8 +7,7 @@ import pandas as pd
 import numpy as np
 
 import Processing.Helpers as H
-import Stats.Survival as Surv
-from Stats.Scipy import rev_kruskal, fisher_exact_test
+from Stats.Scipy import rev_kruskal
 from Stats.Scipy import spearman_pandas
 from Initialization.InitializeReal import exp_change
 
@@ -23,16 +22,17 @@ def mut_filter(df, rate, binary_cutoff=12):
     Filter out mutation features, ensuring that a feature
     is not entirely an artifact of mutation rate.
     """
-    df = df[df.sum(1) >= binary_cutoff]
+    get_min_count = lambda s: s.value_counts().min() if len(s.unique()) > 1 else -1
+    df = df[df.apply(get_min_count, axis=1) > binary_cutoff]
     cc = H.screen_feature(rate, rev_kruskal, df)
-    
+
     fc_apply = lambda s: fc(s, rate)
     direction = df.apply(fc_apply, axis=1)
     direction.name = 'direction'
-    
+
     cc = cc.join(direction)
-    cc = cc[cc.direction == False]
-    
+    cc = cc[cc.direction is False]
+
     df = df.ix[H.true_index(cc.p > .01)]
     df = df.dropna(axis=1)
     return df
@@ -111,7 +111,7 @@ def remove_redundant_pathways(pathways, background, cutoff=.7,
     else:
         binary_pathways = pathways.apply(binarize_feature, 1)
         return binary_pathways
-    
+
 
 def extract_diff_exp_rna(rna, n=300, binarize=False):
     """
@@ -137,10 +137,10 @@ def corrections(vec):
     """
     bonf_all = vec * len(vec)
     bonf_within = vec.groupby(level=0).apply(lambda s: s * len(s))
-    
+
     bh_all = H.bhCorrection(vec)
     bh_within = vec.groupby(level=0).apply(H.bhCorrection).order()
-    
+
     two_step = bh_within * len(vec.groupby(level=0).size())
     q = pd.concat([vec, bh_within, bh_all, bonf_all, bonf_within, two_step],
                   keys=['uncorrected', 'bh_within', 'bh_all', 'bonf_all', 'bonf_within',
@@ -152,29 +152,28 @@ class Screen(object):
     """
     Object to hold data and results for survival screen.
     """
-    
-    def __init__(self, mut, cn, rna, mirna, clinical_df): 
-        
+
+    def __init__(self, mut, cn, rna, mirna, clinical_df):
         """Process Gene / Pathway Expression"""
         pathways = remove_redundant_pathways(rna.pathways, rna.global_vars.background,
                                              binarize=True)
         rna_gene_df = extract_diff_exp_rna(rna, n=300, binarize=True)
         self.rna_df = pd.concat([rna_gene_df, pathways]).T
-        
+
         """Process miRNA Expression"""
         mirna_binarized = mirna.features.ix['real'].apply(binarize_feature, 1)
         self.mirna_df = pd.concat([mirna.features.ix['binary'], mirna_binarized]).T
-        
+
         """Process mutation data"""
         self.rate = mut.df.sum()
         self.mut_df = mut.features
-        
+
         """Process CNA data"""
         self.cna_df = cn.features
-        
+
         """Process Clinical Data"""
         self.clinical_df = clinical_df
-        
+
     def get_patient_set(self, filters):
         f1 = list(filters)
         filter_df = pd.concat(f1, axis=1)
@@ -183,14 +182,14 @@ class Screen(object):
         keepers_o = keepers_o.intersection(self.mut_df.columns)
         keepers_o = keepers_o.intersection(self.cna_df.columns)
         return keepers_o
-        
+
     def get_data(self, keepers_o, cutoff=12):
         mut_df = mut_filter(self.mut_df.ix[:, keepers_o], self.rate,
                             cutoff).T
         amp_df, del_df = cn_filter(self.cna_df.ix[:, keepers_o], cutoff)
         cna_df = pd.concat([del_df, amp_df], keys=['del', 'amp'], axis=1)
         cna_df.columns = map(lambda i: '_'.join(i), cna_df.columns)
-        
+
         df = pd.concat({'clinical': self.clinical_df,
                         'mutation': mut_df,
                         'cna': cna_df,
@@ -205,16 +204,17 @@ class ScreenResult(object):
     """
     Object to hold results for survival screen iteration.
     """
+
     def __init__(self, results, pairs, full, univariate, patients, df):
         self.patients = patients
         self.features = univariate.index
-        
+
         self.univariate = univariate
         self.full = full
         self.pairs = pairs
-        self.results = results 
+        self.results = results
         self.df = df
-        
+
     def __repr__(self):
         hits = sum(self.full.p.bh_all < .1)
         best = self.results.p.uncorrected.order().index[0]
