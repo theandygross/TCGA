5677e8714f63388919b90248d667dbd66aa37765
theandygross <the.andrew.gross@gmail.com>
2014-03-12 11:15:35 -0700

    Fix for new format.

--- src/Data/ProcessClinical.py
+++ src/Data/ProcessClinical.py
@@ -186,7 +186,10 @@ def format_survival(clin, followup):
     timeline = timeline.groupby(level=0).max()
     deceased = timeline.daystodeath.isnull() == False
 
-    days = timeline.days[((timeline.days > 7) | (deceased == False))]
+    #days = timeline.days[((timeline.days > 7) | (deceased == False))]
+    #days = days[days > 0]
+
+    days = timeline.days[timeline.days >= 0]
     survival = pd.concat([days, deceased], keys=['days', 'event'], axis=1)
     survival = survival.dropna().stack().astype(float)
 
